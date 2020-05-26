b922d5a6c948d13526ae49fee9839698b785e82e
Altair <altair.ioffe@gmail.com>
2020-05-26 21:33:21 +0000

    remove dead expression

--- client/src/components/LogoutPrompt.js
+++ client/src/components/LogoutPrompt.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./LogoutPrompt.scss"
 
 export default function logoutPrompt(props){
-
-    const logoutPrompt = "Everybody has the power to remodel their behaviour, habits, and attitudes, but not everybody knows how. Our app will make it simple and rewarding for anybody to get the benefits of reflective journaling. Our app will bring people together through personal goals, challenges, and insights, so that we can realize our potential together."
   
   return (
     <main  id="login-prompt">
