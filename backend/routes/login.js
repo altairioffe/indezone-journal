6fc3317c9a64570ea036d618232e57cda7dc23b8
Altair <altair.ioffe@gmail.com>
2020-05-27 18:12:10 +0000

    fix export glitch in login route

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const { doesEmailExist } = require('./routeHelpers/userHelpers')
+const express = require("express");
+let router = express.Router();
+
+const { doesEmailExist } = require("./routeHelpers/userHelpers");
+
+  //Set cookies
 
 
-module.exports = () => {
-   //Set cookies
-
-
-
-   //get all users
-   router.post("/", (req, res) => {
+  //get all users
+  router.post("/", (req, res) => {
+    console.log("REQ: ", req.body);
     let email = req.body.data.email;
 
-    console.log("HIT POST ROUTE LOGIN: ", rez.body.data.email)
-    db.user.findAll().then(users => {
-      doesEmailExist(email, users)})
+    console.log("HIT POST ROUTE LOGIN: ", req.body.data.email);
+    db.user
+      .findAll()
+      .then(users => {
+        doesEmailExist(email, users);
+      })
+      .then(res => res.json(res))
       .catch(err => {
         res.status(500).json({ error: err.message });
       });
   });
 
-   
-  return router;  
-}
+module.exports = router;
