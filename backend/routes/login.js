64c54d61e3e5bb01170d7826ba580cea96f77875
Altair <altair.ioffe@gmail.com>
2020-05-27 17:28:46 +0000

    add debugging code

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { doesEmailExist } = require('./routeHelpers/userHelpers')
+
+
 module.exports = () => {
    //Set cookies
-   router.get("/", (req, res) => {
-   });
+
+
+
+   //get all users
+   router.post("/", (req, res) => {
+    let email = req.body.data.email;
+
+    console.log("HIT POST ROUTE LOGIN: ", rez.body.data.email)
+    db.user.findAll().then(users => {
+      doesEmailExist(email, users)})
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
    
   return router;  
