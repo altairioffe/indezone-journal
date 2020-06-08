9b28d1ba703e44147a3984a1fd2edcdeff1d257b
Altair <altair.ioffe@gmail.com>
2020-06-08 11:06:54 -0400

    comment out broken code

--- client/src/components/Wall.js
+++ client/src/components/Wall.js
@@ -5,17 +5,21 @@ import  {styles}  from './WallStyles';
 import '../styles/Wall.scss';
 export default function Wall(props) {
   const userGoals = props.userGoals;
-  const userGoalsView = userGoals.map(userGoal => {
+  console.log("FROM WALL: ", userGoals)
+ // const userGoalsView = userGoals.map(userGoal => {
+
+
+let userGoal = {id: 1, createdAt: 2, question: 3, answer: 4}
     return (
       <WallItem key={userGoal.id} createdAt={userGoal.createdAt} question={userGoal.question} answer={userGoal.answer} />
     );
-  }
-  );
+  
+  ;
 
   return (
     <div class ="timeline">
       <Timeline lineStyle={styles.lineStyle}>
-        {userGoalsView}
+        {3}
       </Timeline>
     </div>
   );
