4e2965645893120aea93bb497d569b3ea87dfa22
Altair <altair.ioffe@gmail.com>
2020-06-08 12:12:01 -0400

    wall rendering user entries from database

--- client/src/components/Wall.js
+++ client/src/components/Wall.js
@@ -4,22 +4,25 @@ import { Timeline } from 'react-event-timeline'
 import  {styles}  from './WallStyles';
 import '../styles/Wall.scss';
 export default function Wall(props) {
-  const userGoals = props.userGoals;
-  console.log("FROM WALL: ", userGoals)
- // const userGoalsView = userGoals.map(userGoal => {
+  let userGoals = [];
 
 
-let userGoal = {id: 1, createdAt: 2, question: 3, answer: 4}
-    return (
-      <WallItem key={userGoal.id} createdAt={userGoal.createdAt} question={userGoal.question} answer={userGoal.answer} />
-    );
+  userGoals.push(props.userGoals)
   
-  ;
+  console.log("FROM WALL:, ", userGoals[0])
+  userGoals.forEach(x => console.log(x))
+
+  const userGoalsView = userGoals[0].map(userGoal => {
+    return (
+      <WallItem key={userGoal.id} createdAt={userGoal.createdAt} question={userGoal.goal_id} answer={userGoal.answer} />
+    );
+  }
+  );
 
   return (
     <div class ="timeline">
       <Timeline lineStyle={styles.lineStyle}>
-        {3}
+        {userGoalsView}
       </Timeline>
     </div>
   );
