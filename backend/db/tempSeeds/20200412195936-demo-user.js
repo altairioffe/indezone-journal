890502ec01c2deffbdf21826476c7fcc949b6682
Altair <altair.ioffe@gmail.com>
2020-06-09 12:18:48 -0400

    move conflicting seed files out of db folder

---
+++ backend/db/tempSeeds/20200412195936-demo-user.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity. */
+
+    return queryInterface.bulkInsert('users', [{
+      id:6,
+      handle: '@john',
+      email: 'john@gmail.com',
+      password:'john',
+      points: 1,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    },
+    {
+      id:7,
+      handle: '@ann',
+      email: 'ann@gmail.com',
+      password:'ann',
+      points: 10,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+
+    },
+    {
+      id:8,
+      handle: '@george',
+      email: 'george@gmail.com',
+      password:'george',
+      points: 1,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+
+    },
+    {
+      id:9,
+      handle: '@kim',
+      email: 'kim@gmail.com',
+      password:'kim',
+      points: 1,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+
+    }
+  
+    ]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
