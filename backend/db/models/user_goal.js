69c9ad02afc084ad731ce2c4205c7d462c5762c1
Altair <altair.ioffe@gmail.com>
2020-06-09 12:21:29 -0400

    fix models & migrations for usergoals in database

--- backend/db/models/user_goal.js
+++ backend/db/models/user_goal.js
@@ -3,10 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const UserGoal = sequelize.define('user_goal', {
     goal_id: DataTypes.INTEGER,
     user_id: DataTypes.INTEGER,
-
-    answer: DataTypes.TEXT,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    answer: DataTypes.TEXT
   }, {});
   UserGoal.associate = function(models) {
     // associations can be defined here
