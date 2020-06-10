7ff08ab5d4907ee7c1082993acec816051e744bc
Altair <altair.ioffe@gmail.com>
2020-06-10 11:22:51 -0400

    remove card header style

--- client/src/components/WallStyles.js
+++ client/src/components/WallStyles.js
@@ -1,6 +1,10 @@
 export const  styles = {
-  contentStyle:{backgroundColor:"#FAFAFA"},
-  bubbleStyle:{border:"2px solid #002266"},
+  contentStyle:{
+    backgroundColor:"#FAFAFA"
+    },
+  bubbleStyle:{
+    border:"2px solid #002266",
+    },
   lineColor:"#00008B",
   lineStyle:{ background: '#004d99', width: 5 }
 };
