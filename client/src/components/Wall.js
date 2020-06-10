214176941be12888cfaf071a9d3ea9fc730fce3c
Altair <altair.ioffe@gmail.com>
2020-06-10 11:15:12 -0400

    reformat time display

--- client/src/components/Wall.js
+++ client/src/components/Wall.js
@@ -12,11 +12,12 @@ export default function Wall(props) {
   userGoals[0].sort((a,b) => b.id - a.id)
 
   let questions = props.goals
+
   let userGoalsView = userGoals[0].map(userGoal => {
     let linkedQuestion = questions.filter(question => question.id === userGoal.goal_id)
     let timeStamp = new Date(userGoal.createdAt)
     return (
-      <WallItem key={userGoal.id} createdAt={moment(timeStamp).format('YYYY MMM DD HH:mm')} question={linkedQuestion[0].question} answer={userGoal.answer} />
+      <WallItem key={userGoal.id} createdAt={moment(timeStamp).format('HH:mm MMM DD, YYYY')} question={linkedQuestion[0].question} answer={userGoal.answer} />
     );
   }
   );
