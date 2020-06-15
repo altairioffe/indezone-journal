2911de70470a33e134eb784382b77244261bde8b
Altair <altair.ioffe@gmail.com>
2020-06-15 16:57:11 -0400

    reasonable progress on this ungodly navbar layout

--- client/src/components/LandingPage/Navbar.js
+++ client/src/components/LandingPage/Navbar.js
@@ -11,7 +11,9 @@ import {
   Collapse,
   Grid,
   Card,
-  CardMedia
+  CardMedia,
+  AppBar,
+  Typography
 } from "@material-ui/core";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
@@ -36,6 +38,17 @@ export default function Navbar(props) {
       height: 48,
       padding: "0 30px",
       margin: "0 10px"
+    },
+    navBar: {
+      // height: "40vh",
+      // display: "flex",
+      // width: "200%",
+      // flexDirection: "row"
+    },
+    title: {
+      fontSize: "1em",
+      flexDirection: "row",
+      justify: "flex-end"
     }
   });
   const classes = useStyles();
@@ -60,71 +73,106 @@ export default function Navbar(props) {
   };
 
   return (
-    <Box>
-      <Grid container direction="row" justify="space-between">
-        <Grid item>
-          <img src="images/indezone.png" max-height="2vh" height="20vh" />
-        </Grid>
-        {props.user && (
-          <Grid item xs={4}>
-            <Grid container direction="column">
-              <Grid item justify="flex-end">
-                <Grow in={loginState === 2} timeout={500} unmountOnExit>
-                  <span>
-                    Welcome,{" "}
-                    <strong>{props.user ? props.user.handle : "error"}.</strong>{" "}
-                    Your score:{" "}
-                    <strong style={{ color: "#00A8E0" }}>{props.level}</strong>
-                  </span>
-                </Grow>
-              </Grid>
-              <Grid item justify="flex-end" mr="0">
-                <Grow in={loginState === 2} timeout={500} unmountOnExit>
-                  <Button
-                    onClick={() => logout()}
-                    size="medium"
-                    style={{ textTransform: "none" }}>
-                    | Logout |
-                  </Button>
-                </Grow>
-              </Grid>
-            </Grid>
+    <div>
+      <AppBar
+        display="flex"
+        className="navBar"
+        position="sticky"
+        flexWrap="nowrap">
+
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+          flexGrow={1}>
+          <Grid item xs={6}>
+            <Box
+              p={1}
+              m={0}
+              flexGrow={1}
+              bgcolor="grey.300"
+              style={{ height: "100%", width: "100%", maxWidth: "20vh" }}>
+              <img src="images/indezone.png" height="100%" width="100%" />
+            </Box>
           </Grid>
+
+          <Grid item xs={6}>
+            {props.user && (
+              <Grid item>
+                <Container bgcolor="yellow">
+                <Grid item justify="flex-end" mr="0">
+
+                  <Grow in={loginState === 2} timeout={500} unmountOnExit>
+                    <Typography variant="h6" className={classes.title} align="right">
+                      Welcome,{" "}
+                      <strong>
+                        {props.user ? props.user.handle : "error"}.
+                      </strong>{" "}
+                      Your score:{" "}
+                      <strong style={{ color: "#00A8E0" }}>
+                        {props.level}
+                      </strong>
+                    </Typography>
+                  </Grow>
+                  </Grid>
+                  <Grid item justify="flex-end" mr="0">
+                    <Box
+                      display="flex"
+                      p={0}
+                      m={0}
+                      bgcolor="grey.300"
+                      justifyContent="flex-end">
+                      <Grow in={loginState === 2} timeout={500} unmountOnExit>
+                        <Button
+                          onClick={() => logout()}
+                          size="medium"
+                          style={{ textTransform: "none" }}>
+                          Logout
+                        </Button>
+                      </Grow>
+                    </Box>
+                  </Grid>
+                </Container>
+              </Grid>
+            )}
+          </Grid>
+          {/* LOGOUT */}
+        </Grid>
+      </AppBar>
+      <Box>
+        {/* REGISTRATION */}
+        {props.user === null && (
+          <Collapse
+            direction="left"
+            in={loginState === 0}
+            timeout={500}
+            unmountOnExit>
+            <Register
+              registrationHandler={props.registrationHandler}
+              loginCallback={renderUserDashboard}
+              back={() => setLoginState(1)}
+            />
+          </Collapse>
         )}
-        {/* LOGOUT */}
-      </Grid>
 
-      {/* REGISTRATION */}
-      {props.user === null && (
-        <Collapse
-          direction="left"
-          in={loginState === 0}
-          timeout={500}
-          unmountOnExit>
-          <Register
-            registrationHandler={props.registrationHandler}
-            loginCallback={renderUserDashboard}
-            back={() => setLoginState(1)}
-          />
-        </Collapse>
-      )}
+        {/* LOGIN */}
+        {props.user === null && (
+          <Collapse
+            direction="left"
+            in={loginState === 1}
+            timeout={500}
+            unmountOnExit>
+            <Login
+              loginHandler={props.loginHandler}
+              loginCallback={renderUserDashboard}
+              back={() => setLoginState(0)}
+            />
+          </Collapse>
+        )}
 
-      {/* LOGIN */}
-      {props.user === null && (
-        <Collapse
-          direction="left"
-          in={loginState === 1}
-          timeout={500}
-          unmountOnExit>
-          <Login
-            loginHandler={props.loginHandler}
-            loginCallback={renderUserDashboard}
-            back={() => setLoginState(0)}
-          />
-        </Collapse>
-      )}
-
-      <div></div>
-    </Box>
+        <div></div>
+      </Box>
+    </div>
   );
 }
