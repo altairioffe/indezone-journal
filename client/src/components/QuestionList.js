559224942ae4ff73ea68acf8318fc8c5a79bdde0
Altair <altair.ioffe@gmail.com>
2020-06-22 20:38:42 -0400

    separate long-form question panel

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -33,7 +33,7 @@ export default function QuestionFeed(props) {
     };
   });
   let finalQuestion = props.questions[props.questions.length-1]
-  questionsArr.push({id: finalQuestion.id, question: finalQuestion.question, suggestion: finalQuestion.suggestion})
+  //questionsArr.push({id: finalQuestion.id, question: finalQuestion.question, suggestion: finalQuestion.suggestion})
 
   const questionsFilteredList = questionsArr.map((goal, i) => {
     return (
@@ -58,6 +58,19 @@ export default function QuestionFeed(props) {
     <section>
       <br />
       {questionsFilteredList}
+      <Slide direction="up" in={false}>
+          <AnswerQuestionPanel
+            key={questionsFilteredList.length}
+            question={finalQuestion.question}
+            suggestion={finalQuestion.suggestion}
+            ansQuestion={props.ansQuestion}
+            setAnswer={props.setAnswer}
+            answer={props.answer}
+            goal_id={finalQuestion.id}
+            addUserGoal={props.addUserGoal}
+            currentUserId={props.currentUserId}
+          />
+        </Slide>
       {!questionsFilteredList.length === 0 && (
         <Card className={classes.root} variant="contained-primary">
           <CardContent>
