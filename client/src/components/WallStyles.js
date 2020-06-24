2ba61fc58a480b6fda77740b26adc41e58f90809
Altair <altair.ioffe@gmail.com>
2020-06-24 14:56:08 -0400

    style question & wall components

--- client/src/components/WallStyles.js
+++ client/src/components/WallStyles.js
@@ -4,9 +4,9 @@ export const  styles = {
     opacity:0.5
     },
   bubbleStyle:{
-    border:"2px solid #FAFAFA",
-    backgroundColor:"#FAFAFA"
+    border:"2px solid `rgb(235,240,235, 0.9)`",
+    backgroundColor:`rgb(235,240,235, 0.8)`
     },
-  lineColor:"#00008B",
-  lineStyle:{ background: '#FAFAFA', width: 5 }
+  lineColor:`rgb(235,240,235, 0.8)`,
+  lineStyle:{ background: `rgb(235,240,235, 0.8)`, width: 5, marginLeft: -2.5}
 };
