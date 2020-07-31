c4822ad6a6e246303b59626c114ba60951653683
Altair <altair.ioffe@gmail.com>
2020-07-31 13:19:12 -0400

    fix import for slide 9

--- client/src/components/Rewards/Lessons/Level3/Index.js
+++ client/src/components/Rewards/Lessons/Level3/Index.js
@@ -10,7 +10,7 @@ import Slide5 from "./Slide5.js";
 import Slide6 from "./Slide6.js";
 import Slide7 from "./Slide7.js";
 import Slide8 from "./Slide8.js";
-import Slide8 from "./Slide9.js";
+import Slide9 from "./Slide9.js";
 import Carousel from "react-material-ui-carousel";
 
 export default function Level1(props) {
