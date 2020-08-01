4b96801c82c329d0bf13d059e8cbefdba5662895
Altair <altair.ioffe@gmail.com>
2020-08-01 13:38:32 -0400

    comment out popover in insights

--- client/src/components/Rewards/Insight/Insights.js
+++ client/src/components/Rewards/Insight/Insights.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
 
 export default function Insights(props) {
   // Define Styles
@@ -26,14 +28,62 @@ export default function Insights(props) {
       fontWeight: 300,
       fontSize: "1em",
       padding: "0.5em 2em"
+    },
+    text: {
+      fontWeight: 200,
+      padding: "1em"
     }
   });
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
   return (
-    <article>
-      <h2> Here is my analysis of your entries: </h2>
-      <p style={{ fontWeight: "bold" }}>{props.insights}</p>
+    <article className={classes.text}>
+      <h2> From your answers, I see it is likely that: </h2>
+      <p style={{ margin: "1em" }}>{props.insights}</p>
+      <h6>
+        From this feedback, is there anything that you agree with and would like
+        to work on changing?
+      </h6>
+
+      {/* <div>
+        <Button
+          aria-describedby={id}
+          variant="contained"
+          color="primary"
+          onClick={handleClick}>
+          How is this measured?
+        </Button>
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+          }}>
+          <Typography className={classes.typography}>
+            Wisdom, of course! And an AI algorithm developed by IBM.
+          </Typography>
+        </Popover>
+      </div> */}
       <Button
         className={classes.button}
         onClick={props.onCancel}
