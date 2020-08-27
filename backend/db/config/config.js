57af1c3c57f2fb98cacf4875aec5d3d8a04dcfea
Altair <altair.ioffe@gmail.com>
2020-08-27 13:06:45 -0400

    modify db config for production & procfile

--- backend/db/config/config.js
+++ backend/db/config/config.js
@@ -3,10 +3,21 @@ const path = require('path');
 require('dotenv').config();
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER_DEVELOPMENT,
+    password: process.env.DB_PASS_DEVELOPMENT,
+    database: process.env.DB_NAME_DEVELOPMENT,
+    host: process.env.DB_HOST_DEVELOPMENT,
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.DB_USER_PRODUCTION,
+    password: process.env.DB_PASS_PRODUCTION,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST_PRODUCTION,
     port: 5432,
     dialect: 'postgres',
     dialectOptions: {
